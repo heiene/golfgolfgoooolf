3e8b364fb3e860c6f6aa6f1e8bdf4c02339d3a82
Øyvind Heiene <oyvindheiene@hotmail.com>
2012-07-19 19:09:05 +0200

    Initial (second) commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolfApp::Application.config.secret_token = '36d1e5d22da8a011b058aab190b84f9c730e28013cfdff61ce37d33bda296274cd5dbd86538956cb2fd527a7f8a119fc66f1c0263159bc85710d8ccf58eef8ad'
