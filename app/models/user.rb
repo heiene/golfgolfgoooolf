091b274ed3164a757f3f19d0845c73048007198a
heiene <oyvindheiene@hotmail.com>
2012-08-01 20:53:55 +0200

    halvveis i users fiks

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  global_admin    :boolean
+#  local_admin     :boolean
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  admin           :boolean         default(FALSE)
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+  VALID_EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+            format: { with: VALID_EMAIL_FORMAT }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  before_save do 
+    self.email.downcase!
+    create_remember_token
+  end
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+end
