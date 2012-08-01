0200df26439aa6707ee960aa672f07a31e8635f8
Tor Einar Lyngset <lyngset@gmail.com>
2012-08-02 00:49:31 +0200

    startet med course

---
+++ app/models/course.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: course
+#
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  holes           :integer
+#  slope_value     :float
+
+class Course
+  attr_accessible:
+
+end
